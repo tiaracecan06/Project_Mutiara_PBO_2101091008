a084de519a8963d5eaed31885e118ba61d258159
Mutiara Maharani <114200536+tiaracecan06@users.noreply.github.com>
2022-11-16 12:33:28 +0700

    Add files via upload

---
+++ mutiara04112022/latihan2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mutiara04112022;
+
+/**
+ *
+ * @author Mutiara Maharani
+ */
+public class latihan2 {
+     public static void main( String[] args ){
+         
+         // do while
+         String days[] = {"Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu","Minggu"};
+         
+         int i = 0;
+         do{
+            System.out.println(days[i]);
+            i++;   
+            }while (i < 7);
+        }
+}
