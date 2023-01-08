09ec84d8cc552a52f748e79d309f258c942edfa3
Mutiara Maharani <114200536+tiaracecan06@users.noreply.github.com>
2023-01-08 16:58:01 +0700

    Add files via upload
    Kumpulan File Praktek PBO

---
+++ Praktek_PBO_Mutiara Maharani_2101091008/mutiara30112022/Cat.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mutiara30112022;
+
+/**
+ *
+ * @author Mutiara Maharani
+ */
+public class Cat extends LivingThing{
+    public void walk(){
+        System.out.println("Cat");
+    }
+}
